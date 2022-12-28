02f8f0e3f391265a594e368d38e608447cc4956d
Kalyan <117555665+mail4metablocks@users.noreply.github.com>
2022-12-28 16:46:02 +0530

    Update and rename sample-trend-follower.rs to trend_follower.rs

--- src/trend_follower.rs
+++ src/trend_follower.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-struct TrendFollower {
+pub struct TrendFollower {
     // The number of periods to use for moving average calculation
     periods: usize,
     // The current moving average
@@ -10,7 +10,7 @@ struct TrendFollower {
 }
 
 impl TrendFollower {
-    fn new(periods: usize) -> Self {
+    pub fn new(periods: usize) -> Self {
         Self {
             periods,
             moving_average: 0.0,
@@ -18,7 +18,7 @@ impl TrendFollower {
         }
     }
 
-    fn update(&mut self, price: f64) {
+    pub fn update(&mut self, price: f64) {
         // Add the new price to the history
         self.price_history.push_back(price);
         // If we have more prices than the number of periods, remove the oldest price
@@ -30,12 +30,12 @@ impl TrendFollower {
         self.moving_average = sum / self.price_history.len() as f64;
     }
 
-    fn should_buy(&self, price: f64) -> bool {
+    pub fn should_buy(&self, price: f64) -> bool {
         // If the current price is above the moving average, it may be a good time to buy
         price > self.moving_average
     }
 
-    fn should_sell(&self, price: f64) -> bool {
+    pub fn should_sell(&self, price: f64) -> bool {
         // If the current price is below the moving average, it may be a good time to sell
         price < self.moving_average
     }
