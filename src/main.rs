6ff54f5f69617f2c4f9fc9fae352f76ad8d772de
Kalyan <117555665+mail4metablocks@users.noreply.github.com>
2022-12-28 16:46:43 +0530

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+mod trend_follower;
+
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("Usage: trend_follower <periods> <price1> <price2> ... <priceN>");
+        std::process::exit(1);
+    }
+
+    let periods = args[1].parse::<usize>().unwrap();
+    let prices: Vec<f64> = args[2..].iter().map(|s| s.parse::<f64>().unwrap()).collect();
+
+    let mut trend_follower = trend_follower::TrendFollower::new(periods);
+
+    for price in prices {
+        trend_follower.update(price);
+        println!("Current price: {} Moving average: {}", price, trend_follower.moving_average);
+    }
+}
